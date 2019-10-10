62db2bb152e2db7caa7658c945958ccae0edd708
aliciawongg <w.alicia@gmail.com>
2019-10-10 23:01:16 +0800

    add function to create card

--- public/index.js
+++ public/index.js
@@ -1,49 +1,46 @@
+// column component
 class MyColumn extends HTMLElement {
     constructor() {
         super();
-        
     }
 }
 
 customElements.define('my-column', MyColumn)
-
-const url1 = 'http://localhost:3000/columns'
 
 window.addEventListener('load', ()=> {
     createColumn();
 })
 
 async function createColumn() {
-    const response = await fetch(url1);
+    const response = await fetch('http://localhost:3000/columns');
     const json = await response.json();
     console.log(json);
 
     json.forEach((column) => {
         console.log(column.title);
         const newColumn = document.createElement('my-column');
-        newColumn.id = `${column.title}`;
+        newColumn.id = `${column.title}`
         newColumn.innerHTML = `<h2>${column.title}</h2>`;
         document.querySelector('.container').appendChild(newColumn);
+        
     })
 }
 
+// card component
 class MyCard extends HTMLElement {
     constructor() {
         super();
-        
     }
 }
 
 customElements.define('my-card', MyCard)
-
-const url2 = 'http://localhost:3000/cards'
 
 window.addEventListener('load', ()=> {
     getCardInfo();
 })
 
 async function getCardInfo() {
-    const response = await fetch(url2);
+    const response = await fetch('http://localhost:3000/cards');
     const json = await response.json();
     console.log(json);
 
@@ -58,4 +55,28 @@ async function getCardInfo() {
             document.getElementById('Column 2').appendChild(shadowRoot);
         }
     })
+}
+// create card
+//document.getElementById('createCard').addEventListener('submit', createCard);
+function createCard(event) {
+
+    let title = document.getElementById('title').value
+    let description = document.getElementById('description').value
+    let columnId = document.getElementById('columnId').value
+
+    fetch('http://localhost:3000/cards', {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+        method: 'POST',
+        body: JSON.stringify({
+          title: title,
+          description: description,
+          columnId: columnId
+        }),
+        
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
 }
